48daf471135d9281703ccbce7b72dec68542e7ca
unknown <eden.abdisa@yahoo.com>
2021-03-10 15:42:50 +0300

    adjusted version

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/users','UserController@index');
+Route::post('/users','UserController@store');
+Route::post('/users/login','UserController@login');
